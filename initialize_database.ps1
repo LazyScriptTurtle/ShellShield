106eb3693121885c3a5403ca39255eae17dc8355
LazyScriptTurtle <162825856+LazyScriptTurtle@users.noreply.github.com>
2025-08-01 22:24:15 +0200

    add Domains Table

--- initialize_database.ps1
+++ initialize_database.ps1
@@ -6,7 +6,7 @@ function Initialize-Database {
         Write-Host "Creating new database: $DatabasePath" -ForegroundColor Yellow
         
         # Create database file
-        $createTableQuery = @"
+        $createIocTableQuery = @"
 CREATE TABLE IF NOT EXISTS IOC (
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
     IP TEXT UNIQUE NOT NULL,
@@ -15,10 +15,39 @@ CREATE TABLE IF NOT EXISTS IOC (
     SOURCE TEXT
 );
 "@
+        $createHashesTableQuery = @"
+CREATE TABLE "HASHES" (
+	"ID"	INTEGER NOT NULL UNIQUE,
+	"NAME"	TEXT,
+	"TYPE"	TEXT,
+	"SHA256"	TEXT UNIQUE,
+	"SHA1"	TEXT UNIQUE,
+	"MD5"	TEXT UNIQUE,
+	"FIRST_SEEN"	TEXT,
+	"ORIG_COUNTRY"	TEXT,
+	"SOURCE"	TEXT,
+	PRIMARY KEY("ID")
+);
+"@
+        $createDomainTableQuery = @"
+CREATE TABLE "DOMAINS" (
+	"ID"	INTEGER NOT NULL UNIQUE,
+	"DOMAIN"	TEXT NOT NULL UNIQUE,
+	"TIMESTAMP"	TEXT,
+	"STATUS"	TEXT,
+	"LAST_SEEN"	TEXT,
+	"THREAT"	TEXT,
+	"SOURCES"	TEXT,
+	PRIMARY KEY("ID")
+);
+"@
         
-        Invoke-SqliteQuery -DataSource $DatabasePath -Query $createTableQuery
+        Invoke-SqliteQuery -DataSource $DatabasePath -Query $createIoCTableQuery
+        Invoke-SqliteQuery -DataSource $DatabasePath -Query $createHashesTableQuery
+        Invoke-SqliteQuery -DataSource $DatabasePath -Query $createDomainTableQuery
         Write-Host "Database created successfully!" -ForegroundColor Green
-    } else {
-        Write-Host "Database already exists: $DatabasePath" -ForegroundColor Green
+    }
+    else {
+        Write-Host "Databases already exists: $DatabasePath" -ForegroundColor Green
     }
 }
